52cfca1b4e5715ef4b8702522ad17909a1525dea
saidbukhariakbarmaulana <108089850+SaidBukhariAkbarMaulana@users.noreply.github.com>
2023-04-12 17:12:01 +0800

    posttest6

---
+++ Posttest6/src/posttest6/Tambahan.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package posttest6;
+
+/**
+ *
+ * @author LENOVO
+ */
+public interface Tambahan {
+    String Kewarganegaraan();
+    String berlakuHingga();
+}
